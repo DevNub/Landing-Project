33759cf58c13c77c670b4c3c577d3f96b3f59714
DevNub <ddac_dan@hotmail.com>
2018-12-22 11:55:03 -0500

    javascript achievement list added
    derp

---
+++ script/script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    //Part1
+    //get social buttons /mote elements
+    //find a way to move them randomly, within a limited space around the main logo
+
+    //Part 2
+    //Blurry Panoramic Background Image Loop
+    //there must be a way lol
+
+    //Part 3 - Final
+    //Pulsating and glow effect on social icons and main logo
+    //implement by changing icon names to their pulse and glow style
+    //but this has to be placed in a function that loops the changes at a moderate pace o.0
+}
